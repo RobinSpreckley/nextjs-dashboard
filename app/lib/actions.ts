bfbb35a62734534c51252a893132b17606034efc
Robin Spreckley <jspreckley12@gmail.com>
2025-11-24 20:12:07 +0000

    enviroment URL fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 import { error } from 'console';
 
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_PRISMA_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
